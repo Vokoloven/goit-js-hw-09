1dca084d7c046ada2d635f7c69d469eef7ac040b
Ruslan Volovenko <vokoloven@gmail.com>
2022-09-10 00:11:55 +0300

    Completed first task

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+  btnStart: document.querySelector('button[data-start]'),
+  btnStop: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+let timerId = 0;
+
+refs.btnStart.addEventListener('click', () => {
+  refs.btnStart.disabled = 'disabled';
+  refs.btnStop.removeAttribute('disabled');
+
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = getRandomHexColor();
+  }, 500);
+});
+
+refs.btnStop.addEventListener('click', () => {
+  refs.btnStop.disabled = 'disabled';
+  refs.btnStart.removeAttribute('disabled');
+  clearInterval(timerId);
+});
